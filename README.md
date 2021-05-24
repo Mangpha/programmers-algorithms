d516158c09c6323d5da91deaade1dca397dc3783
Mangpha <8350130@gmail.co>
2021-05-25 00:50:00 +0900

    an_unfinished~.py

---
+++ README.md
@@ -0,0 +1,5 @@
+### Programmers.co.kr
+
+# 풀이 완료한 문제 코드 모음
+
+an_unfinished_athlete.py => 완주하지 못한 선수
---
+++ an_unfinished_athlete.py
@@ -0,0 +1,23 @@
+# 1
+
+
+def solution(participant, completion):
+    for i in range(len(completion)):
+        if completion[i] in participant:
+            participant.remove(completion[i])
+    return "".join(participant)
+
+
+# 2
+
+from collections import Counter
+
+
+def solution2(participant, completion):
+    return list(Counter(participant) - Counter(completion))[0]
+
+
+p1 = ["mislav", "stanko", "mislav", "ana"]
+c1 = ["stanko", "ana", "mislav"]
+
+print(solution2(p1, c1))
