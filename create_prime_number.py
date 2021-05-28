3e6e7e9efbd60ffcc8af31fd67a770123cbbf05d
Mangpha <8350130@gmail.co>
2021-05-28 20:29:01 +0900

    소수 만들기

--- create_prime_number.py
+++ create_prime_number.py
@@ -2,10 +2,11 @@ import itertools
 
 
 def checking(nums):
+    # 소수를 구하는데 0, 1은 필요없기 때문에 return
     if nums == 0 or nums == 1:
         return False
     for i in range(2, int(nums ** (0.5)) + 1):
-
+        # ex) num = 10 : nums ** 0.5 = 3.16... +1을 해줘야 range값에 포함 시킬 수 있음
         if nums % i == 0:
             return False
 
