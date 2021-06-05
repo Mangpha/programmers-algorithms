b0d3704009269e9ddbe50890c8359910a70398fd
Mangpha <8350130@gmail.co>
2021-06-05 22:46:12 +0900

    문자열 내 p와 y의 개수

--- str_p_y.py
+++ str_p_y.py
@@ -1,3 +1,6 @@
+# filter로 p가 들어있는 배열의 길이와 y가 들어있는 배열의 길이를 비교한다
+
+
 def solution(s):
     p = len(list(filter(lambda x: x == "p", s.lower())))
     y = len(list(filter(lambda x: x == "y", s.lower())))
